1b54de682054ba0941c2c6db3da27cd7068dd214
Amadeuszzyla <amadeusz.zyla@studenci.collegiumwitelona.pl>
2026-01-24 08:41:38 +0100

    dodano logo

--- frontend/src/shims.d.ts
+++ frontend/src/shims.d.ts
@@ -4,6 +4,11 @@ declare module '*.vue' {
     export default component
 }
 
+declare module '*.svg' {
+    const src: string
+    export default src
+}
+
 declare module 'granim'
 
 declare module '@/api/axios' {
